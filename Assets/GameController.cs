8f09ad3e044eb595bd8d7c46a4464c4688459f4c
Luke Brown <thelukedbrown@gmail.com>
2017-04-08 17:29:50 -0400

    Initial Commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class GameController : MonoBehaviour
+{
+
+	public GameObject multiTarget;
+	private bool targetInitialized;
+	private BasicTrackableEventHandler eventHandler;
+
+	// Use this for initialization
+	void Start ()
+	{
+		targetInitialized = false;
+		eventHandler = multiTarget.GetComponent<BasicTrackableEventHandler> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!targetInitialized && eventHandler.isTracking) {
+			targetInitialized = true;
+			Debug.Log ("Target Initialized");
+		}
+
+	}
+}
