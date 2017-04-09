345f9d20983320c4bd10fd0af78d6791aadeb333
Cody Claborn <codylclaborn@gmail.com>
2017-04-08 20:20:28 -0400

    Added destorying enemy ship

--- Assets/Scripts/EnemyShip.cs
+++ Assets/Scripts/EnemyShip.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyShip : MonoBehaviour {
 
@@ -17,23 +18,47 @@ public class EnemyShip : MonoBehaviour {
     private int Health;
     [SerializeField]
     private int MaxHealth = 5;
+    [SerializeField]
+    private bool IsDead;
 
 	// Use this for initialization
 	void Start () {
+        IsDead = false;
         Health = MaxHealth;
         City = GameObject.FindObjectOfType<City>();
         DamageTimer = gameObject.AddComponent<Timer>();
         DamageTimer.StartTimer(Random.Range(MinFireTime, MaxFireTime), 1, OnTimerComplete);
+
+        var gaze = gameObject.GetComponent<BasicGazeButton>();
+        gaze.OnGazeInput.AddListener(OnGazeHit);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (IsDead)
+        {
+            gameObject.SetActive(false);
+        }
 	}
 
     void OnTimerComplete(Timer t)
     {
         DamageTimer.StartTimer(Random.Range(MinFireTime, MaxFireTime), 1, OnTimerComplete);
         City.DamageCity(DamagePower);
+    }
+
+    public void HitShip(int damage)
+    {
+        Debug.Log("Ship HIT!!!");
+        Health -= damage;
+        if (Health <= 0)
+        {
+            IsDead = true;
+        }
+    }
+
+    public void OnGazeHit()
+    {
+        HitShip(1);
     }
 }
